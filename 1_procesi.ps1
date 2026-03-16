97e87f936d372051a3e55553aeaa19fa9211cd93
R1viks <103327127+R1viks@users.noreply.github.com>
2026-03-16 14:14:22 +0200

    Update 1_procesi.ps1

--- 1_procesi.ps1
+++ 1_procesi.ps1
@@ -1,7 +1,8 @@
 $Arhivs="$env:C:\Users\A250644RV\Desktop\1_procesi.ps1"
 
-Get-Process | Where-Object WS -gt 150MB -ne "msedge"
-
+Get-Process | Where-Object {
+$_.workingSet -gt 150MB -and $_.Name -ne "msedge"
+}
 # Export.csv
 Get-Process | Select-Object -Property Name, Id, WS
 
