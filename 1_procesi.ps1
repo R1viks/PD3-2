1ea1c2594f8e0c8fd24ef698407c52a5a129eb57
R1viks <103327127+R1viks@users.noreply.github.com>
2026-03-19 11:30:19 +0200

    Add files via upload

--- 1_procesi.ps1
+++ 1_procesi.ps1
@@ -1,7 +1,5 @@
-$Arhivs="$env:C:\Users\A250644RV\Desktop\1_procesi.ps1"
-
 Get-Process | Where-Object {
     $_.WorkingSet -gt 150MB -and $_.Name -ne "msedge"
 } | Select Name, Id, WorkingSet |
-Export-Csv "$env:USERPROFILE\Documents\LielieProcesi.csv" -NoTypeInformation
+Export-Csv "$env:C:\Users\A250644RV\Documents\LielieProcesi.csv" 
 
