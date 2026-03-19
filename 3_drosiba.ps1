4ab85c58ef61404f1518ecb0df6a52a072c4001e
R1viks <103327127+R1viks@users.noreply.github.com>
2026-03-19 10:19:47 +0200

    Add files via upload

--- 3_drosiba.ps1
+++ 3_drosiba.ps1
@@ -3,5 +3,6 @@ $s = Get-MpComputerStatus
 
 if ($s.RealTimeProtectionEnabled -eq $false) { $score -= 50 }
 if ($s.AntivirusSignatureAge -gt 3) { $score -= 20 }
-if ($s.QuickScanAge -gt 7) 
+if ($s.QuickScanAge -gt 7) { $score = 20 }
+"Sistemas drosibas reitings: $score / 100"
 
