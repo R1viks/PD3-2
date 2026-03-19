03fb41e2695469dffbca08c754406493238b3de9
R1viks <103327127+R1viks@users.noreply.github.com>
2026-03-19 10:07:02 +0200

    Update 3_drosiba.ps1

--- 3_drosiba.ps1
+++ 3_drosiba.ps1
@@ -1,1 +1,7 @@
 $score = 100
+$s = Get-MpComputerStatus
+
+if ($s.RealTimeProtectionEnabled -eq $false) { $score -= 50 }
+if ($s.AntivirusSignatureAge -gt 3) { $score -= 20 }
+if ($s.QuickScanAge -gt 7) 
+
