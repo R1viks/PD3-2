6ec36504c1b563575e6c41c4055a16f54a922124
R1viks <103327127+R1viks@users.noreply.github.com>
2026-03-19 11:02:37 +0200

    Add files via upload

--- 4_arhivs.ps1
+++ 4_arhivs.ps1
@@ -1,5 +1,5 @@
-$pdf = Get-ChildItem "$env:USERPROFILE\Downloads\*.pdf" | Where-Object {
+$pdf = Get-ChildItem "$env:C:\Users\A250644RV\Downloads\*.pdf" | Where-Object {
     $_.LastWriteTime -gt (Get-Date).AddHours(-48)
 }
 
-Compress-Archive $pdf.FullName "$env:USERPROFILE\Documents\PDF_Backup.zip" -Force
+Compress-Archive $pdf.FullName "$env:C:\Users\A250644RV\Documents\PDF_Backup.zip" -Force
